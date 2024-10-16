31714ff20766916a6114c1bde2cad89cee677a6d
bluetree00 <bg63645539@gmail.com>
2024-10-17 04:51:53 +0900

    +
    Former-commit-id: 737791e6f224401dce8258ecfd159607847576f1

--- Assets/02.Scripts/Manager/ObjectPoolerManager.cs
+++ Assets/02.Scripts/Manager/ObjectPoolerManager.cs
@@ -6,16 +6,15 @@ public class ObjectPoolerManager
 {
     private Dictionary<string, Queue<GameObject>> poolDictionary;
     private List<GameObject> spawnObjects;
+    private Pool[] pools;
 
     [Serializable]
     public class Pool
     {
         public string tag;
-        public GameObject prefab;
-        public int size;
+        public string resourcePath; // 리소스 매니저에서 사용할 경로
+        public int initialSize;
     }
-
-    private Pool[] pools;
 
     public ObjectPoolerManager(Pool[] pools)
     {
@@ -26,51 +25,94 @@ public class ObjectPoolerManager
         foreach (Pool pool in pools)
         {
             poolDictionary[pool.tag] = new Queue<GameObject>();
-            for (int i = 0; i < pool.size; i++)
+
+            for (int i = 0; i < pool.initialSize; i++)
             {
-                var obj = CreateNewObject(pool.tag, pool.prefab);
-                ArrangePool(obj);
+                GameObject obj = CreateNewObject(pool.tag, pool.resourcePath);
+                ReturnToPool(obj); // 초기 오브젝트를 풀에 추가
             }
         }
     }
 
-    private GameObject CreateNewObject(string tag, GameObject prefab)
+    private GameObject CreateNewObject(string tag, string resourcePath)
     {
-        var obj = UnityEngine.Object.Instantiate(prefab);
+        GameObject prefab = Managers.Resource.Load<GameObject>($"Prefabs/{resourcePath}");
+        if (prefab == null)
+        {
+            Debug.LogError($"Prefab at path {resourcePath} not found.");
+            return null;
+        }
+
+        GameObject obj = GameObject.Instantiate(prefab);
         obj.name = tag;
         obj.SetActive(false);
-        return obj;
-    }
-
-    private void ArrangePool(GameObject obj)
-    {
         spawnObjects.Add(obj);
-        poolDictionary[obj.name].Enqueue(obj);
+        return obj;
     }
 
     public GameObject SpawnFromPool(string tag, Vector3 position, Quaternion rotation)
     {
         if (!poolDictionary.ContainsKey(tag))
-            throw new Exception($"Pool with tag {tag} doesn't exist.");
-
-        Queue<GameObject> poolQueue = poolDictionary[tag];
-        if (poolQueue.Count <= 0)
         {
-            Pool pool = Array.Find(pools, x => x.tag == tag);
-            var obj = CreateNewObject(pool.tag, pool.prefab);
-            ArrangePool(obj);
+            throw new Exception($"Pool with tag {tag} doesn't exist.");
         }
 
-        GameObject objectToSpawn = poolQueue.Dequeue();
+        if (poolDictionary[tag].Count == 0)
+        {
+            // 풀에 오브젝트가 부족할 경우 새 오브젝트 생성 후 추가
+            Pool pool = Array.Find(pools, x => x.tag == tag);
+            if (pool != null)
+            {
+                GameObject obj = CreateNewObject(pool.tag, pool.resourcePath);
+                if (obj != null)
+                    poolDictionary[tag].Enqueue(obj);
+            }
+        }
+
+        GameObject objectToSpawn = poolDictionary[tag].Dequeue();
         objectToSpawn.transform.position = position;
         objectToSpawn.transform.rotation = rotation;
         objectToSpawn.SetActive(true);
+
         return objectToSpawn;
+    }
+
+    public T SpawnFromPool<T>(string tag, Vector3 position, Quaternion rotation) where T : Component
+    {
+        GameObject objectToSpawn = SpawnFromPool(tag, position, rotation);
+        if (objectToSpawn.TryGetComponent(out T component))
+        {
+            return component;
+        }
+        else
+        {
+            ReturnToPool(objectToSpawn);
+            throw new Exception($"Component {typeof(T)} not found on pooled object with tag {tag}");
+        }
     }
 
     public void ReturnToPool(GameObject obj)
     {
         obj.SetActive(false);
-        poolDictionary[obj.name].Enqueue(obj);
+        if (!poolDictionary.ContainsKey(obj.name))
+        {
+            Debug.LogWarning($"Pool with tag {obj.name} doesn't exist. Object destroyed instead of returned to pool.");
+            GameObject.Destroy(obj);
+        }
+        else
+        {
+            poolDictionary[obj.name].Enqueue(obj);
+        }
+    }
+
+    public void LogPoolStatus()
+    {
+        foreach (var pool in pools)
+        {
+            if (poolDictionary.ContainsKey(pool.tag))
+            {
+                Debug.Log($"{pool.tag} Pool: {poolDictionary[pool.tag].Count} / {spawnObjects.Count}");
+            }
+        }
     }
 }
