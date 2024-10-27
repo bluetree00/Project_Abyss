22109a18c6cf4e8034bea2f7aa136b46315a97be
bluetree00 <bg63645539@gmail.com>
2024-10-27 20:45:48 +0900

    스크립터블 데이터 직렬화
    Former-commit-id: 252625d161cfe823e022d0a04c2febcee6e4d727

--- Assets/02.Scripts/Data/ScriptableObject/PlayerData/CharacterData.cs
+++ Assets/02.Scripts/Data/ScriptableObject/PlayerData/CharacterData.cs
@@ -7,4 +7,13 @@ public class CharacterData : ScriptableObject
     public float baseRunSpeed;
     public int maxHealth;
     public int attackPower;
+    
+    public int attackComboStep = 0;       // 공격 스택 단계
+    public float comboTimer = 0.0f;       // 콤보 유지 시간
+    public float comboDuration = 3.0f;     // 콤보가 유지되는 시간
+
+    public bool canDodge = true;          // 대시 가능 여부
+    public float dashSpeed = 10f;          // 대시 속도
+    public float dashDuration = 0.2f;      // 대시 지속 시간
+    public float dodgeCooldown = 2f;       // 대시 쿨타임
 }
