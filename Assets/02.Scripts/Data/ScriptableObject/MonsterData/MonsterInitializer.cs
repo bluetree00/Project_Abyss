b54565c470356f262325ba4a0f73328976e9d374
bluetree00 <bg63645539@gmail.com>
2024-10-30 04:52:37 +0900

    각 타입별 에디더 수정
    Former-commit-id: d0cb0795c5ea58b7c40cc0f865db01ff09481c47

---
+++ Assets/02.Scripts/Data/ScriptableObject/MonsterData/MonsterInitializer.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MonsterInitializer
+{
+    public static List<ObjectPoolerManager.Pool> GetInitialPools(string monsterPoolDataName)
+    {
+        // 스크립터블 오브젝트를 이름으로 로드
+        MonsterPoolData monsterPoolData = Resources.Load<MonsterPoolData>($"Data/{monsterPoolDataName}");
+        
+        if (monsterPoolData == null)
+        {
+            Debug.LogError($"EffectPoolData with name {monsterPoolDataName} not found.");
+            return new List<ObjectPoolerManager.Pool>();
+        }
+
+        List<ObjectPoolerManager.Pool> pools = new List<ObjectPoolerManager.Pool>();
+
+        foreach (var pool in monsterPoolData.pools)
+        {
+            pools.Add(new ObjectPoolerManager.Pool
+            {
+                tag = pool.tag,
+                resourcePath = pool.resourcePath,
+                initialSize = pool.initialSize,
+                poolType = pool.poolType
+            });
+        }
+
+        return pools;
+    }
+}
