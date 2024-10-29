3da72a1f3d98e2212d8e73c0ee103a9bedf14d47
bluetree00 <bg63645539@gmail.com>
2024-10-30 04:52:37 +0900

    각 타입별 에디더 수정
    Former-commit-id: d0cb0795c5ea58b7c40cc0f865db01ff09481c47

---
+++ Assets/02.Scripts/Data/ScriptableObject/MonsterData/MonsterPoolData.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewEffectPoolData", menuName = "Pools/MonsterPoolData")]
+public class MonsterPoolData : ScriptableObject
+{
+    [Tooltip("List of object pools with their respective settings.")]
+    public List<PoolSettings> pools; // 풀 목록을 담는 리스트
+
+    [System.Serializable]
+    public class PoolSettings
+    {
+        [Tooltip("Unique identifier tag for this pool.")]
+        public string tag; // 풀의 태그
+
+        [Tooltip("Path to the prefab under Resources/Prefabs folder.")]
+        public string resourcePath; // 리소스 경로
+
+        [Tooltip("Initial number of objects to instantiate for this pool.")]
+        public int initialSize; // 초기 오브젝트 수
+
+        [Tooltip("Type of pool, used to assign to specific categories such as effects, monsters, etc.")]
+        public ObjectPoolerManager.PoolType poolType; // 풀 타입
+    }
+}
