5ed40e918ce1ce2f9c2843daa8b41bb47cd040a5
bluetree00 <bg63645539@gmail.com>
2024-11-05 16:14:56 +0900

    +
    Former-commit-id: 42136b9c032062e8243823f3d177ccd58171cdf8

--- Assets/02.Scripts/Manager/ObjectPoolerManager.cs
+++ Assets/02.Scripts/Manager/ObjectPoolerManager.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectPoolerManager
+public class ObjectPoolerManager 
 {
     private Dictionary<string, Queue<GameObject>> poolDictionary;
     private List<GameObject> spawnObjects;
