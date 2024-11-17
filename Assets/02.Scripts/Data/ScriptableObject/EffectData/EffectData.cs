be5d9282b158b28aa21f04b4835371bcdd4239cb
loremtho <133847848+loremtho@users.noreply.github.com>
2024-11-18 01:12:29 +0900

    공격력 데이터 연동 다시 시작
    Former-commit-id: b3a1a64871b51e6ca4bd0dcdd690083d56da370f

--- Assets/02.Scripts/Data/ScriptableObject/EffectData/EffectData.cs
+++ Assets/02.Scripts/Data/ScriptableObject/EffectData/EffectData.cs
@@ -9,6 +9,8 @@ public class EffectData : ScriptableObject
     public string effectTag; // 이펙트 태그 추가
     public bool isPlayerEffect; // 플레이어용 이펙트 여부
 
+    // 다른 변수 ex) 캐릭터 데미지 + 이펙트 데미지
+
     // 외부에서 데미지를 증가시키는 메서드
     public void IncreaseDamage(float amount)
     {
@@ -20,4 +22,6 @@ public class EffectData : ScriptableObject
     {
         damage = newDamage; // 새로운 데미지로 설정
     }
+
+    // 이 데이터 스크립트 안에서 캐릭터 데이터와 이펙트 데이터를 연결하는 함수
 }
