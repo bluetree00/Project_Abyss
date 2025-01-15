5f1d3b5fb5d68681ee640bcfdf8065177918b9bf
BeomKyuKim <bg63645539@gmail.com>
2025-01-16 04:32:22 +0900

    그리드 드래그 드롭 로직 구현
    Former-commit-id: 01938e52a14e2602774be86ddf75aa53fc872dc4

--- Assets/Resources/UseResources/PuzzleUISource/PuzzleScripts/Game/GridSquare.cs
+++ Assets/Resources/UseResources/PuzzleUISource/PuzzleScripts/Game/GridSquare.cs
@@ -5,17 +5,54 @@ using UnityEngine.UI;
 
 public class GridSquare : MonoBehaviour
 {
+    public Image hooverImage;
+    public Image activeImage;
     public Image normalImage;
     public List<Sprite> normallmages;
+
+    public bool Selected {get; set;}
+    public int SquareIndex{get; set;}
+    public bool SquareOccupied {get; set;}
     // Start is called before the first frame update
     void Start()
     {
+        Selected = false;
+        SquareOccupied = false;
         
     }
+
+    public bool CanWeUseThisSquare()
+    {
+        return hooverImage.gameObject.activeSelf;
+    }
+
+    public void ActivateSquare()
+    {
+        hooverImage.gameObject.SetActive(false);
+        activeImage.gameObject.SetActive(true);
+        Selected = true;
+        SquareOccupied = true;
+    }
+
 
     public void SetImage(bool setFirstImage)
     {
         normalImage.GetComponent<Image>().sprite = setFirstImage ? normallmages[1] : normallmages[0];
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        hooverImage.gameObject.SetActive(true);
+    }
+
+     private void OnTriggerStay2D(Collider2D other) 
+    {
+        hooverImage.gameObject.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D other) 
+    {
+        hooverImage.gameObject.SetActive(false);
     }
 
 }
