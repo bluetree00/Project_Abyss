db5afa6ae00a96ddef825464e2fab788c9e486ec
loremtho <133847848+loremtho@users.noreply.github.com>
2025-01-23 23:21:27 +0900

    해제한 무기 메모리에서 제거 함수 추가, 무기가 없을 경우 무기변경 못함
    Former-commit-id: 3cd046b6c8a06b7229a965efe380b471cec016bf

--- Assets/02.Scripts/Manager/AddressableManager.cs
+++ Assets/02.Scripts/Manager/AddressableManager.cs
@@ -1,12 +1,23 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
 
+[Serializable]
+public class LoadedAsset
+{
+    public string key;
+    public AsyncOperationHandle handle;
+}
+
 public class AddressablesManager : MonoBehaviour
 {
+    private Dictionary<string, AsyncOperationHandle> loadedAssets = new Dictionary<string, AsyncOperationHandle>();
+    [SerializeField]
+    private List<LoadedAsset> loadedAssetsList = new List<LoadedAsset>();
     private static AddressablesManager _instance;
     public static AddressablesManager Instance
     {
@@ -22,6 +33,8 @@ public class AddressablesManager : MonoBehaviour
         }
     }
 
+    
+
     private void Start()
     {
         StartCoroutine(InitAddressables());
@@ -31,6 +44,15 @@ public class AddressablesManager : MonoBehaviour
     {
         var Init = Addressables.InitializeAsync();
         yield return Init;
+    }
+
+    private void UpdateLoadedAssetsList()
+    {
+        loadedAssetsList.Clear();
+        foreach (var kvp in loadedAssets)
+        {
+            loadedAssetsList.Add(new LoadedAsset { key = kvp.Key, handle = kvp.Value });
+        }
     }
     
     /// <summary>
@@ -65,11 +87,49 @@ public class AddressablesManager : MonoBehaviour
     {
         if (handle.Status == AsyncOperationStatus.Succeeded)
         {
+            loadedAssets[key] = handle;
+            UpdateLoadedAssetsList();
             onSuccess?.Invoke(handle.Result);
         }
         else
         {
             onFailure?.Invoke();
+        }
+    }
+
+    /// <summary>
+    /// 어드레서블 시스템으로 로드했던 리소스를 해제하는 함수
+    /// </summary>
+    /// <param name="key">해제할 리소스의 키</param>
+    public void ReleaseAsset(string key)
+    {
+        if (loadedAssets.TryGetValue(key, out AsyncOperationHandle handle))
+        {
+            Addressables.Release(handle);
+            Debug.Log($"<color=orange>리소스 데이터 : {key} 를 해제 </color>");
+            loadedAssets.Remove(key);
+            UpdateLoadedAssetsList();
+        }
+        else{
+            Debug.LogError("해당 키의 리소스가 없습니다.");
+        }
+    }
+
+    /// <summary>
+    /// 인스턴스화하여 생성시켰던 오브젝트를 삭제하는 함수
+    /// </summary>
+    /// <param name="key">삭제할 오브젝트의 키</param>
+    public void ReleaseInstance(string key)
+    {
+        if (loadedAssets.TryGetValue(key, out AsyncOperationHandle handle))
+        {
+            Addressables.ReleaseInstance(handle);
+            Debug.Log($"<color=orange>오브젝트 : {key} 를 삭제 </color>");
+            loadedAssets.Remove(key);
+            UpdateLoadedAssetsList();
+        }
+        else{
+            Debug.LogError("해당 키의 오브젝트가 없습니다.");
         }
     }
 }
