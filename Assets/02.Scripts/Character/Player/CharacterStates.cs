e1dc4e6b523b7fd2fc06dbf8e545f55ed6156879
bluetree00 <bg63645539@gmail.com>
2025-03-21 16:24:14 +0900

    상태 머신 0.2
    Former-commit-id: bdd5b6d3efccbb5d5796212953013116c305043c

--- Assets/02.Scripts/Character/Player/CharacterStates.cs
+++ Assets/02.Scripts/Character/Player/CharacterStates.cs
@@ -5,18 +5,29 @@ namespace Game.CharacterStates.States
 
     // 기본 상속할 상태 클래스들
 
-    public class IdleState<T> : State<T> where T : CharacterController
+   public class IdleState<T> : State<T> where T : CharacterController
     {
         public override void Enter(T owner)
         {
             Debug.Log($"{typeof(T).Name} - Idle 상태 진입");
 
+            // 무기 장착 여부 및 애니메이션 이름에 따라 애니메이션 설정
+            if (owner.WeaponContainer != null &&
+                owner.WeaponContainer.isWeaponEquipped &&
+                owner.currentWeapon != null &&
+                !string.IsNullOrEmpty(owner.currentWeapon.weapon_Idle_AnimationName))
+            {
+                owner.Anim.CrossFade(owner.currentWeapon.weapon_Idle_AnimationName, 0.2f);
+            }
+            else
+            {
+                owner.Anim.CrossFade("Idle", 0.2f);
+            }
         }
 
         public override void Execute(T owner)
         {
-            // 기본 Idle 상태 로직
-             owner.Anim?.CrossFade("Idle", 0.2f);
+            // Idle 상태에서 지속 로직이 필요한 경우 여기에
         }
 
         public override void Exit(T owner)
@@ -42,6 +53,21 @@ namespace Game.CharacterStates.States
         public override void Exit(T owner) { }
     }
 
+    public class RuningState<T> : State<T> where T : CharacterController
+    {
+        public override void Enter(T owner)
+        {
+           
+        }
+
+        public override void Execute(T owner)
+        {
+            owner.Anim?.CrossFade("Runing", 0.1f);
+        }
+
+        public override void Exit(T owner) { }
+    }
+
     public class DodgeState<T> : State<T> where T : CharacterController
     {
         public override void Enter(T owner)
@@ -51,7 +77,6 @@ namespace Game.CharacterStates.States
 
         public override void Execute(T owner)
         {
-        // owner.Move(owner.moveDirection, owner.CharacterData.baseMoveSpeed);
         
         }
 
@@ -67,7 +92,6 @@ namespace Game.CharacterStates.States
 
         public override void Execute(T owner)
         {
-          // owner.Move(owner.moveDirection, owner.CharacterData.baseMoveSpeed);
         }
 
         public override void Exit(T owner) { }
