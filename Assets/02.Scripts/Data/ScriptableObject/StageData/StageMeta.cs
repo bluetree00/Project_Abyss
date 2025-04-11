0ccffba4c064d28eda8ed9ff9df42277e5d6b7ff
loremtho <133847848+loremtho@users.noreply.github.com>
2025-04-12 01:11:46 +0900

    어드레서블 프리펩 정보 불러와서 MST 트리 시퀀스 만들기
    Former-commit-id: b6eab48ce2b7f244f80a00cf36f0c0780896a807

--- Assets/02.Scripts/Data/ScriptableObject/StageData/StageMeta.cs
+++ Assets/02.Scripts/Data/ScriptableObject/StageData/StageMeta.cs
@@ -7,5 +7,6 @@ public class StageMeta : MonoBehaviour
     public string stageName;
     public StageManager.StageType stageType;
     public int weight; // 초기 값, 이후 MST 생성 시 덮어씀
+    public string label;
 }
 
