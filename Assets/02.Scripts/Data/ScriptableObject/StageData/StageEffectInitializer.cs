b4fd08b646cc7fc0d7d4ff01d78f931d7a3ad514
loremtho <133847848+loremtho@users.noreply.github.com>
2025-04-13 19:35:56 +0900

    이벤트 스테이지 Data SO
    Former-commit-id: 8f661f104991b9caf3483dcc9a8a4afa4b2b06b1

--- Assets/02.Scripts/Data/ScriptableObject/StageData/StageEffectInitializer.cs
+++ Assets/02.Scripts/Data/ScriptableObject/StageData/StageEffectInitializer.cs
@@ -7,12 +7,13 @@ using UnityEngine.AddressableAssets;
 
 public static class StageEffectInitializer
 {
-    public static async Task<(List<StageManager.Stage>, List<StageManager.ConnectionRestriction>, string)> GetInitialStagesForChapterAsync(string chapterSOName)
+    public static async Task<(List<StageManager.Stage>, List<StageManager.ConnectionRestriction>, string, Dictionary<EventStageData.EventStageType, List<StageManager.Stage>>)> GetInitialStagesForChapterAsync(string chapterSOName)
     {
         // StageData 로드
         //StageData stageData = Resources.Load<StageData>($"Data/{chapterSOName}");
 
         string chapterFileName = "Data/" + chapterSOName;
+        string eventStageLabel = "Event";
 
         var handle = Addressables.LoadAssetsAsync<StageData>(chapterFileName, null);
         IList<StageData> stageDataList = await handle.Task;
@@ -21,7 +22,7 @@ public static class StageEffectInitializer
         if (stageDataList == null || stageDataList.Count == 0)
         {
             Debug.LogError($"No StageData found with name {chapterSOName}.");
-            return (null, null, null);
+            return (null, null, null, null);
         }
 
         // 첫 번째 StageData 사용
@@ -29,6 +30,7 @@ public static class StageEffectInitializer
 
         // 결과 목록 초기화
         List<StageManager.Stage> stages = new List<StageManager.Stage>();
+        Dictionary<EventStageData.EventStageType, List<StageManager.Stage>> eventStageDic = new Dictionary<EventStageData.EventStageType, List<StageManager.Stage>>();
         List<StageManager.ConnectionRestriction> restrictions = new List<StageManager.ConnectionRestriction>();
         string bossStageName = stageData.chapters[0].bossStageName;
         string chapterName = stageData.chapters[0].chapterName.ToString();
