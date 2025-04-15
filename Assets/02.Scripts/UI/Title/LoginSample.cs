0581b7b1e288a9b5f00db2ece0fb9c7e6326ca55
bluetree00 <bg63645539@gmail.com>
2025-04-15 18:29:05 +0900

    로그인 기능 완성
    Former-commit-id: f40042856393d9147da48444e372c949ea443d3d

--- Assets/02.Scripts/UI/Title/LoginSample.cs
+++ Assets/02.Scripts/UI/Title/LoginSample.cs
@@ -5,26 +5,17 @@ public class LoginSample : MonoBehaviour
 {
     private void Awake()
     {
-        string ID = "testID"; // 사용자 ID
-        string PW = "testPW"; // 사용자 비밀번호
-        string email = "testEmail"; // 사용자 이메일
-        string nickname = "testNickname"; // 사용자 닉네임
+        string ID = "user01"; // 사용자 ID
+        string PW = "1234"; // 사용자 비밀번호
+        string email = "user01@gmail.com"; // 사용자 이메일    
+        string nickname = "첫번째유저"; // 사용자 닉네임
 
         Backend.BMember.CustomSignUp(ID,PW);
 
         Backend.BMember.UpdateCustomEmail(email);
         
-        Backend.BMember.CustomLogin(ID, PW, (login) =>
-        {
-            if (login.IsSuccess())
-            {
-                Debug.Log("Login Success: " + login.GetMessage());
-            }
-            else
-            {
-                Debug.LogError("Login Failed: " + login.GetMessage());
-            }
-        });
+        Backend.BMember.CustomLogin(ID, PW);
+
 
         Backend.BMember.FindCustomID(email);
 
