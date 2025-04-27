bdee0e29799af85fe145cb3f0b529d232c562c58
bluetree00 <bg63645539@gmail.com>
2025-04-27 18:41:03 +0900

    어빌리티 시스템 최적화 공격 장비별 대응 시작.
    Former-commit-id: 3384480a3c6481c7e15d10fe44c10dbe69970ed3

--- Assets/02.Scripts/Character/Player/Interfaces/IComboAttackProvider.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Game.CharacterStates;
-
-public interface IComboAttackProvider
-{
-    StateMachine<CharacterController> CreateAttackStateMachine(CharacterController owner);
-}
