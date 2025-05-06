bbb3fe90402e24f42a4fb4fe1815aa374ee3886b
bluetree00 <bg63645539@gmail.com>
2025-05-07 03:59:57 +0900

    +
    Former-commit-id: 839dff56c123a5c24bc2f4f5e3446e8c04e0f862

--- Assets/02.Scripts/Character/Player/Weapon/WeaponData.cs
+++ Assets/02.Scripts/Character/Player/Weapon/WeaponData.cs
@@ -17,6 +17,12 @@ public abstract class WeaponData : ScriptableObject
     [Header("무기 타입")] // 타입을 이름 기준으로 풀러 연결 초기화화 기본 이펙트를 정해줌. 추후 수정가능 
     public Define.WeaponType weaponType;
 
+    public DefaultLightAttackAbility lightAttackSO;
+    public DefaultHeavyAttackAbility heavyAttackSO;
+
+    public ILightAttackAbility<CharacterController> LightAttack => lightAttackSO;
+    public IHeavyAttackAbility<CharacterController> HeavyAttack => heavyAttackSO;
+
     [Header("무기 등급")]
     public Define.WeaponRarity rarity;
     public int upgradeLevel;
