a6ab35c37ef151e0d119c369284c6982fc6a63ed
bluetree00 <bg63645539@gmail.com>
2025-05-22 15:14:05 +0900

    +
    Former-commit-id: ef2735fe39e3506a59f3c30d3e77745b18d93a78

--- Assets/02.Scripts/Character/Monster/Slime/SlimeMonster.cs
+++ Assets/02.Scripts/Character/Monster/Slime/SlimeMonster.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SlimeMonster : MonoBehaviour
+public class SlimeMonster : MonsterController
 {
     // Start is called before the first frame update
     void Start()
