b63b0ed91f09f1eacd788d77e964343d917838a0
김범규 <bg63645539@gmail.com>
2025-06-03 20:10:19 +0900

    + 기존 상속 코드 구조 변경 시작 최상위 상속을 통한 공통 초기화
    Former-commit-id: 908af1707befaf2731025c2418c2b7764f0f0063

---
+++ Assets/02.Scripts/Character/Monster/MonsterInterfaces/BTMonsterAIController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BTMonsterAIController : IMonsterAIController
+{
+    public void Initialize()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Tick()
+    {
+        throw new System.NotImplementedException();
+    }
+
+  
+}
