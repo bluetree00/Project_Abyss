46ee72a89cae30f57b31d6dbb1ebb56f91088481
김범규 <bg63645539@gmail.com>
2025-09-24 17:05:00 +0900

    WeaponSO 플레이어 이벤트 리시버 작업 중
    Former-commit-id: e42cbd9274d6d5347b4fcee6d608d766e439dfc3

--- Assets/12.Characters/Player/Knight/Scripts/State/LayerFSM/ActAttackReadyState.cs
+++ Assets/12.Characters/Player/Knight/Scripts/State/LayerFSM/ActAttackReadyState.cs
@@ -11,7 +11,7 @@ public class ActAttackReadyState : ILayerState<ActState>
     {
         // 컨텍스트별(지상/공중) 애니 세트 교체
         var isAir = !_controller.IsGrounded();
-        _controller.ApplyAttackAnimationSet(isAir);
+
 
         // 이동 제어(풀보디 공격 기준; 상체공격이면 0.2f 같은 감속으로)
         _controller.AcquireMoveLock();
