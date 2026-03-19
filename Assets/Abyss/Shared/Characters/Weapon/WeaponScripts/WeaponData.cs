7e1c816440e2dc149735ebf4dd11e6ddfc03fc06
KimBeomKyu <bg63645539@gmail.com>
2026-03-19 21:22:02 +0900

    Revert "merge: dev/KBG-N → develop 통합"
    This reverts commit e90c7da01a4263e600f1353e0729a03f3e80eaf6, reversing
    changes made to 0af07be03ee602c883d90b768c9ad7ff93de2933.

--- Assets/Abyss/Shared/Characters/Weapon/WeaponScripts/WeaponData.cs
+++ Assets/Abyss/Shared/Characters/Weapon/WeaponScripts/WeaponData.cs
@@ -2,6 +2,8 @@
 using System;
 using UnityEngine;
 
+public enum WeaponSlotType { Main, Sub }
+
 public enum PromoteMode
 {
     None,       // 차지/강화 없음
@@ -33,8 +35,8 @@ public class WeaponData
 
     public WeaponAnimationSetSO animationSet;
     public WeaponAbilitySetSO abilitySet;
-    public WeaponType     weaponType = WeaponType.None;
-    public WeaponSlotType slotType   = WeaponSlotType.Main;
+    public WeaponType weaponType = WeaponType.None;
+    public WeaponSlotType slotType = WeaponSlotType.Main;
     public string skillName;
     public string skillDescription;
 
@@ -62,10 +64,10 @@ public class WeaponData
         promoteMode = so.promoteMode;
         chargeStages = so.chargeStages;
 
-        weaponType       = so.weaponType;
-        slotType         = so.slotType;
-        skillName        = so.skillName;
-        skillDescription = so.skillDescription;
+        weaponType        = so.weaponType;
+        slotType          = so.slotType;
+        skillName         = so.skillName;
+        skillDescription  = so.skillDescription;
 
         groundEndCount = so.groundEndCount;
         airEndCount = so.airEndCount;
